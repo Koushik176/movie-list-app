3f92896a26bfb7569d6394a13b58b25f59b9f57e
Koushik <koushikashok176@gmail.com>
2023-08-06 12:31:39 +0530

    Loading Icon while the loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch('https://swapi.py4e.com/api/films/').then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+  const [isLoading, setIsloading] = useState(false);
+  async function fetchMoviesHandler() {
+    setIsloading(true);
+    const response = await fetch("https://swapi.py4e.com/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
     });
+    setMovies(transformedMovies);
+    setIsloading(false);
   }
 
   return (
@@ -27,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading..</p>}
       </section>
     </React.Fragment>
   );
